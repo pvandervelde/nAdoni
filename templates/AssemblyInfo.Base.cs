33b4db09a19db83dd140354b45bd206ed4e8a1d8
Petrik van der Velde <petrikvandervelde@gmail.com>
2013-08-07 21:46:57 +1200

    Adding the templates for the AssemblyInfo files.

---
+++ templates/AssemblyInfo.Base.cs
@@ -0,0 +1,19 @@
+//-----------------------------------------------------------------------
+// <copyright company="NAnicitus">
+//     Copyright 2013 NAnicitus. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("P. van der Velde")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
